3751417a1dfd809d63cbcbca18af928ec12a4060
prabhanjansp <puranikchetan97@gmail.com>
2025-09-23 21:01:52 +0530

    Episode -01

--- App.js
+++ App.js
@@ -1,3 +1,26 @@
+/**
+ * <div id="parent">
+ *
+ * <div id=parent>
+ * <h1 id="heading">Namaste React</h1>
+ * </div>
+ *
+ * </div>
+ */
+
 const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
+console.log(heading);
+//heading is react element object or normal js object
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { id: "heading" }, "Namaste React Child"),
+    React.createElement("h2", { id: "heading2" }, "Namaste React Child2"),
+  ]),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { id: "heading" }, "Namaste React Child"),
+    React.createElement("h2", { id: "heading2" }, "Namaste React Child2"),
+  ]),
+]);
+console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
