1c0cca24ab56aed95106228062b6ca23bb16fdbd
prabhanjansp <puranikchetan97@gmail.com>
2025-10-06 11:57:49 +0530

    concepts related to the props ,destructure,props as variable,swiggy api response as json,map and conditional operator and key vs index

--- App.js
+++ App.js
@@ -1,29 +1,615 @@
-/**
- * <div id="parent">
- *
- * <div id=parent>
- * <h1 id="heading">Namaste React</h1>
- * </div>
- *
- * </div>
- */
 import React from "react";
 import ReactDOM from "react-dom/client";
-// const heading = React.createElement("h1", { id: "heading" }, "Hello World from React");
+const card = {
+  backgroundColor: "#f0f0f0",
+};
 
-const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
-console.log(heading);
-//heading is react element object or normal js object
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "heading" }, "Namaste React Child"),
-    React.createElement("h2", { id: "heading2" }, "Namaste React Child2"),
-  ]),
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "heading" }, "Namaste React Child"),
-    React.createElement("h2", { id: "heading2" }, "Namaste React Child2"),
-  ]),
-]);
-console.log(parent);
+const HeaderComponent = () => {
+  return (
+    <div className="header">
+      <div>
+        <img
+          className="logo"
+          src="https://img.freepik.com/premium-vector/food-ordering-app-logo-with-points-fork-shapes-center_666184-195.jpg"
+          alt="logo"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>Cart</li>
+          <li>contact</li>
+          <li>About us</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+const resObj = {
+  "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+  info: {
+    id: "1046102",
+    name: "Tasty Talk",
+    cloudinaryImageId:
+      "RX_THUMBNAIL/IMAGES/VENDOR/2025/2/16/7cbbb087-5eb9-4464-bce2-8e8a392d19a2_1046102.jpg",
+    locality: "Chhindwara City",
+    areaName: "Chhindwara City",
+    costForTwo: "₹200 for two",
+    cuisines: [
+      "Chinese",
+      "Italian",
+      "Pizzas",
+      "Pastas",
+      "Fast Food",
+      "Burgers",
+      "Cafe",
+    ],
+    avgRating: 4.3,
+    veg: true,
+    parentId: "618935",
+    avgRatingString: "4.3",
+    totalRatingsString: "13",
+    sla: {
+      deliveryTime: 77,
+      lastMileTravel: 12.1,
+      serviceability: "SERVICEABLE",
+      slaString: "75-85 mins",
+      lastMileTravelString: "12.1 km",
+      iconType: "ICON_TYPE_EMPTY",
+    },
+    availability: {
+      nextCloseTime: "2025-10-06 23:30:00",
+      opened: true,
+    },
+    badges: {
+      imageBadges: [
+        {
+          imageId: "v1695133679/badges/Pure_Veg111.png",
+          description: "pureveg",
+        },
+      ],
+    },
+    isOpen: true,
+    aggregatedDiscountInfoV2: {},
+    type: "F",
+    badgesV2: {
+      entityBadges: {
+        imageBased: {
+          badgeObject: [
+            {
+              attributes: {
+                description: "pureveg",
+                imageId: "v1695133679/badges/Pure_Veg111.png",
+              },
+            },
+          ],
+        },
+        textBased: {},
+        textExtendedBadges: {},
+      },
+    },
+    orderabilityCommunication: {
+      title: {},
+      subTitle: {},
+      message: {},
+      customIcon: {},
+    },
+    differentiatedUi: {
+      displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+      differentiatedUiMediaDetails: {
+        mediaType: "ADS_MEDIA_ENUM_IMAGE",
+        lottie: {},
+        video: {},
+      },
+    },
+    reviewsSummary: {},
+    displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    restaurantOfferPresentationInfo: {},
+    externalRatings: {
+      aggregatedRating: {
+        rating: "--",
+      },
+    },
+    ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+  },
+  analytics: {
+    context: "seo-data-3e7877ca-cdfe-4914-86fd-bdf84c47de92",
+  },
+  cta: {
+    link: "https://www.swiggy.com/city/chhindwara/tasty-talk-chhindwara-city-rest1046102",
+    text: "RESTAURANT_MENU",
+    type: "WEBLINK",
+  },
+  widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+};
+
+const resList = [
+  {
+    "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+    info: {
+      id: "830418",
+      name: "Indian Coffee House",
+      cloudinaryImageId: "2469fa88ee9b0b5d1366ba88f2a7fa7f",
+      locality: "Chhindwara City",
+      areaName: "Khajri Chowk",
+      costForTwo: "₹300 for two",
+      cuisines: ["South Indian", "Chinese", "Fast Food", "Beverages"],
+      avgRating: 4.5,
+      veg: true,
+      parentId: "106448",
+      avgRatingString: "4.5",
+      totalRatingsString: "264",
+      sla: {
+        deliveryTime: 53,
+        lastMileTravel: 13.1,
+        serviceability: "SERVICEABLE",
+        slaString: "50-60 mins",
+        lastMileTravelString: "13.1 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-10-06 22:30:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "Rxawards/_CATEGORY-South%20Indian.png",
+            description: "Delivery!",
+          },
+          {
+            imageId: "v1695133679/badges/Pure_Veg111.png",
+            description: "pureveg",
+          },
+        ],
+      },
+      isOpen: true,
+      aggregatedDiscountInfoV2: {},
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "Delivery!",
+                  imageId: "Rxawards/_CATEGORY-South%20Indian.png",
+                },
+              },
+              {
+                attributes: {
+                  description: "pureveg",
+                  imageId: "v1695133679/badges/Pure_Veg111.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {
+      context: "seo-data-a7d80463-2273-4ede-a0c8-377bfdd92468",
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/chhindwara/indian-coffee-house-city-khajri-chowk-rest830418",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+    info: {
+      id: "1003414",
+      name: "Pizza Hut",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2025/9/1/bd7954b5-a431-4726-b2da-4670ceba472d_1003414.JPG",
+      locality: "Chhindwara",
+      areaName: "Chhindwara City",
+      costForTwo: "₹350 for two",
+      cuisines: ["Pizzas"],
+      avgRating: 4.3,
+      parentId: "721",
+      avgRatingString: "4.3",
+      totalRatingsString: "187",
+      sla: {
+        deliveryTime: 59,
+        lastMileTravel: 14.1,
+        serviceability: "SERVICEABLE",
+        slaString: "55-65 mins",
+        lastMileTravelString: "14.1 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-10-07 00:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "ITEMS",
+        subHeader: "AT ₹98",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {
+      context: "seo-data-a7d80463-2273-4ede-a0c8-377bfdd92468",
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/chhindwara/pizza-hut-chhindwara-city-rest1003414",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+    info: {
+      id: "1046102",
+      name: "Tasty Talk",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2025/2/16/7cbbb087-5eb9-4464-bce2-8e8a392d19a2_1046102.jpg",
+      locality: "Chhindwara City",
+      areaName: "Chhindwara City",
+      costForTwo: "₹200 for two",
+      cuisines: [
+        "Chinese",
+        "Italian",
+        "Pizzas",
+        "Pastas",
+        "Fast Food",
+        "Burgers",
+        "Cafe",
+      ],
+      avgRating: 4.3,
+      veg: true,
+      parentId: "618935",
+      avgRatingString: "4.3",
+      totalRatingsString: "13",
+      sla: {
+        deliveryTime: 67,
+        lastMileTravel: 12.1,
+        serviceability: "SERVICEABLE",
+        slaString: "65-75 mins",
+        lastMileTravelString: "12.1 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-10-06 23:30:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "v1695133679/badges/Pure_Veg111.png",
+            description: "pureveg",
+          },
+        ],
+      },
+      isOpen: true,
+      aggregatedDiscountInfoV2: {},
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "pureveg",
+                  imageId: "v1695133679/badges/Pure_Veg111.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {
+      context: "seo-data-a7d80463-2273-4ede-a0c8-377bfdd92468",
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/chhindwara/tasty-talk-chhindwara-city-rest1046102",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+    info: {
+      id: "234875",
+      name: "Adil Hotel",
+      cloudinaryImageId: "gp1ityra6utvzqn6ghnv",
+      locality: "Rautha Wada",
+      areaName: "Chhindwara Locality",
+      costForTwo: "₹150 for two",
+      cuisines: ["Biryani", "Tandoor"],
+      avgRating: 4.4,
+      parentId: "27123",
+      avgRatingString: "4.4",
+      totalRatingsString: "1.4K+",
+      sla: {
+        deliveryTime: 57,
+        lastMileTravel: 12.6,
+        serviceability: "SERVICEABLE",
+        slaString: "55-65 mins",
+        lastMileTravelString: "12.6 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-10-07 00:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      aggregatedDiscountInfoV2: {},
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {
+      context: "seo-data-a7d80463-2273-4ede-a0c8-377bfdd92468",
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/chhindwara/adil-hotel-rautha-wada-chhindwara-locality-rest234875",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+
+  {
+    "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+    info: {
+      id: "719963",
+      name: "Chatori Food Junction",
+      cloudinaryImageId: "906c0affa7d8ff78a3e4db88b4433cde",
+      locality: "Parasia Road",
+      areaName: "Parasia Road",
+      costForTwo: "₹200 for two",
+      cuisines: ["South Indian", "Snacks", "Pizzas", "Burgers"],
+      avgRating: 5,
+      veg: true,
+      parentId: "429054",
+      avgRatingString: "5.0",
+      totalRatingsString: "<3",
+      sla: {
+        deliveryTime: 73,
+        lastMileTravel: 14.4,
+        serviceability: "SERVICEABLE",
+        slaString: "70-80 mins",
+        lastMileTravelString: "14.4 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-10-06 23:00:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "v1695133679/badges/Pure_Veg111.png",
+            description: "pureveg",
+          },
+        ],
+      },
+      isOpen: true,
+      aggregatedDiscountInfoV2: {},
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "pureveg",
+                  imageId: "v1695133679/badges/Pure_Veg111.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {
+      context: "seo-data-a7d80463-2273-4ede-a0c8-377bfdd92468",
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/chhindwara/chatori-food-junction-parasia-road-rest719963",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+];
+const RestroCardComponent = (props) => {
+  const { resObj } = props;
+  const {
+    cloudinaryImageId,
+    name,
+    cuisines,
+    avgRating,
+    costForTwo,
+    deliveryTime,
+  } = resObj?.info;
+  return (
+    <div className="res-card" style={card}>
+      <img
+        className="res-logo"
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+          cloudinaryImageId
+        }
+        alt="biriyani"
+      />
+      <h3> {name}</h3>
+      <h4>{cuisines.join(",")}</h4>
+      <h4>{avgRating} stars</h4>
+      <h4>{costForTwo}</h4>
+      <h4>{resObj.info.sla.deliveryTime}min</h4>
+    </div>
+  );
+};
+
+const BodyComponent = () => {
+  return (
+    <div className="body">
+      <div className="search">search</div>
+      <div className="res-container">
+        {/* restaurant cards */}
+
+        {resList.map((restaurant) => {
+          return (
+            <RestroCardComponent key={restaurant.info.id} resObj={restaurant} />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <HeaderComponent />
+      <BodyComponent />
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<AppLayout />);
+//way to render the component
