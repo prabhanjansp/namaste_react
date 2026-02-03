92b09ff8ba2e4950a1f671433708dd1a6f91d24e
prabhanjansp <puranikchetan97@gmail.com>
2026-02-04 05:21:24 +0530

    cors proxy is the way to handle the cors issues without using the extensions

--- src/components/BodyComponent.jsx
+++ src/components/BodyComponent.jsx
@@ -13,7 +13,7 @@ const BodyComponent = () => {
   console.log("rendered");
   const fetchData = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9029202&lng=77.4950581&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",
+      "https://corsproxy.io/?url=https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9029202&lng=77.4950581&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",
     );
     const json = await data.json();
     console.log("json", json);
