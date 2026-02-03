4db4e82e60ff34e95ffa9407ec603708034a7b13
prabhanjansp <puranikchetan97@gmail.com>
2026-02-03 06:28:18 +0530

    implemented the use effect with resolving the cors issues along with the loading and conditional rendering

--- src/components/BodyComponent.jsx
+++ src/components/BodyComponent.jsx
@@ -1,9 +1,25 @@
 import RestroCardComponent from "./RestroCardComponent";
-import resList from "../utils/mockData";
-import { useState } from "react";
+// import resList from "../utils/mockData";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const BodyComponent = () => {
-  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
+  console.log("rendered");
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9029202&lng=77.4950581&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log("json", json);
+    setListOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle.restaurants);
+  };
+  if (listOfRestaurants.length === 0) {
+    return <Shimmer/>
+  }
   return (
     <div className="body">
       {/* <div className="search">search</div> */}
@@ -23,7 +39,7 @@ const BodyComponent = () => {
       <div className="res-container">
         {/* restaurant cards */}
 
-        {listOfRestaurants.map((restaurant) => {
+        {listOfRestaurants?.map((restaurant) => {
           return (
             <RestroCardComponent key={restaurant.info.id} resObj={restaurant} />
           );
